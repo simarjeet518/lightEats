5e1400ea274313bafb7ada77ca03d64c4879bdc9
Allenzzp <131452abc@gmail.com>
2022-01-09 05:05:15 +0000

    implememt 5 routes

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 8080;
 const sassMiddleware = require("./lib/sass-middleware");
 const express = require("express");
 const app = express();
-const morgan = require("morgan");
+//const morgan = require("morgan");
 
 // PG database client/connection setup
 const { Pool } = require("pg");
@@ -17,10 +17,39 @@ db.connect();
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
-app.use(morgan("dev"));
+//app.use(morgan("dev"));
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+
+//helper functions
+const getClientOrders = (id) => {
+  return db.query(`
+  SELECT * 
+  FROM orders
+  JOIN orders_items ON orders.id = orders_items.order_id
+  JOIN menu_items ON menu_items.id = menu_item_id
+  WHERE user_id = $1 
+  `, [id])
+  .then(res => {
+    return res.rows;
+  })
+  .catch(err => err.message);
+}
+
+const getRestaurantOrders = (id) => {
+  return db.query(`
+  SELECT * 
+  FROM orders
+  JOIN orders_items ON orders.id = orders_items.order_id
+  JOIN menu_items ON menu_items.id = menu_item_id
+  WHERE restaurant_id = $1 
+  `, [id])
+  .then(res => {
+    return res.rows;
+  })
+  .catch(err => err.message);
+}
 
 app.use(
   "/styles",
@@ -49,8 +78,82 @@ app.use("/api/widgets", widgetsRoutes(db));
 // Separate them into separate routes files (see above).
 
 app.get("/", (req, res) => {
-  res.render("index");
+  //sql
+  const templatevars = {
+  };
+  res.render("index", templatevars);
 });
+
+app.get("/login", (req, res) => {
+  res.render('login');
+});
+//clinet
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  //sql query get userObj{id}
+  if (userObj.id === "restaurat_owner_id") {
+    res.redirect("/admin/:restaurantid");
+  } else {
+    res.redirect("/");
+  }
+});
+
+
+//client orders page
+app.get("/:userid/orders", (req, res) => {
+  const id = req.params.userid;
+  const clinetOrders = getClientOrders(id);
+  clinetOrders
+  .then(res => {
+    const ordersArray = res;
+    const templatevars = {
+      ordersArray
+    };
+    res.render("orders", templatevars);
+  })
+});
+
+//check shopping cart
+app.get("/:userid/cart", (req, res) => {
+  res.render("cart");
+});
+//submit order
+app.post("/:userid/cart", (req, res) => {
+  //date => insert into table orders, orders_items
+  res.redirect("/:userid/orders");
+});
+
+//restaurant admin page
+app.get("/admin/:restaurantid", (req, res) => {
+  const id = req.params.restaurantid;
+  const newOrders = getRestaurantOrders(id);
+  newOrders
+  .then(res => {
+    const newOrdersArray = res;
+    const templatevars = {
+      newOrdersArray
+    };
+    res.render("admin", templatevars);
+  })
+});
+
+//when click on Accept/DONE
+app.post("/admin/:restaurantid/:function", (req, res) => {
+  const id = req.params.restaurantid;
+  if (req.params.function === 'accpet') {
+    //send msm to clinet
+    //update clinet orders page <time> button
+    return;
+  }
+  if (req.params.function === 'done') {
+    //send msm to client
+    //INSERT INTO orders table SET timestamp2 = NOW();
+    //current order moved to past orders
+    return;
+  }
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
