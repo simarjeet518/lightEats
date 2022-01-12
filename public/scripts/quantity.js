48e9a35f1f717dafd98d0684d62e3eafbb56ba68
Kevin Tan Yulo <yulokevin@gmail.com>
2022-01-11 16:45:08 -0800

    mentor refactored

--- public/scripts/quantity.js
+++ public/scripts/quantity.js
@@ -1,88 +1,38 @@
 /* eslint-disable no-undef */
 
-$(() => {
-  loadOnCents();
+$(document).ready(()=> {
+
+  $('.opt-btn').on('click', function () {
+    const type = $(this).attr('data-type');
+    const cartPrice = $(this).closest('.cart-box-item').find('.cart-price');
+    const quantityBox = $(this).closest('.cart-box-item').find('.qty-box');
+    const iPrice = $(this).closest('.cart-box-item').find('.iprice');
+
+    const unitPrice = Number(cartPrice.text());
+    const currentQuantity = type === 'minus' ? Number(quantityBox.val()) - 1 : Number(quantityBox.val()) + 1;
+    if (currentQuantity >= 0) {
+      quantityBox.val(currentQuantity);
+      iPrice.text((unitPrice * currentQuantity).toFixed(2));
+    }
+
+    updateSubTotal();
+
+  });
+
   updateSubTotal();
-
-  const addition = (variables) => {
-    let idx = variables.data.index;
-    let pricePerItem = variables.data.perPrice;
-    let a = $('#qty-box' + idx).val();
-
-    if (a && a >= 0) {
-      a++;
-      let subTotal = 0;
-      subTotal = a * pricePerItem;
-      $("#qty-box" + idx).val(a);
-      $('#val' + idx).text(subTotal.toFixed(2));
-    }
-
-    updateSubTotal();
-  };
-
-  const subtract = (variables) => {
-    let idx = variables.data.index;
-    let pricePerItem = variables.data.perPrice;
-    let b = $('#qty-box' + idx).val();
-
-    if (b && b >= 1) {
-      b--;
-      let subTotal = 0;
-      subTotal = b * pricePerItem;
-      $("#qty-box" + idx).val(b);
-      $('#val' + idx).text(subTotal.toFixed(2));
-    }
-
-    updateSubTotal();
-
-  };
-
-  $('#button-set-order-time').on('click', confirmCart);
-
-  for (let i = 0; i < 7; i++) {
-    $(".inc" + i).on('click', {index:i, perPrice: Number($('#ppi' + i).text())}, addition);
-    $(".dec" + i).on('click', {index:i, perPrice: Number($('#ppi' + i).text())}, subtract);
-  }
-
 });
 
-const updateSubTotal = () => {
-  let addsum = 0;
-  let cents = 0;
-  for (let k = 0; k < 7; k++) {
-    addsum += Number($('#val' + k).text());
-  }
-  $('#subtotal').text(addsum.toFixed(2));
+
+const updateSubTotal = ()=>{
+  let subTotal = 0;
+  $('.iprice').each(function() {
+    subTotal += Number($(this).text());
+  });
+
+  $("#subtotal").text(subTotal.toFixed(2));
 };
 
-const loadOnCents = () => {
-  for (let k = 0; k < 7; k++) {
-    let p = Number($('#ppi' + k).text()) / 100;
-    $('#ppi' + k).text(p.toFixed(2));
+const quantity = () => {
 
-    let c = Number($('#qty-box' + k).val());
-    cents = c * Number($('#val' + k).text()) / 100;
-    $('#val' + k).text(cents.toFixed(2));
-  }
-};
 
-const confirmCart = () => {
-  // use this for table insert
-  let cart = {};
-  let table = [];
-  const order = {};
-
-  for (let i = 0; i < 7; i++) {
-    if ($('#item-name' + i).text()) { //if, exists insert into table
-      order.name = $('#item-name' + i).text();
-      order.number = $('#qty-box' + i).val();
-
-      table.push(order);
-    }
-
-  }
-  cart.items = table;
-  cart.total = $('#subtotal').text();
-
-  console.log(cart);
-};
+}
