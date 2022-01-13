37aee61c973610e8d2adab2f4841a601a6b4253b
Allenzzp <131452abc@gmail.com>
2022-01-13 19:33:30 +0000

    delete layout.css

--- db/seeds/sample_data.sql
+++ db/seeds/sample_data.sql
@@ -6,39 +6,11 @@ VALUES ('Kevin' , 'kevin@gmail.com','6725151337'),
 INSERT INTO restaurants (name, phone, street, city, province, country, post_code)
 VALUES ('West 12 Cafe', '7788334525','7237 137 St', 'Surrey', 'BC', 'Canada', 'V3H 29T');
 
--- INSERT INTO orders (customer_id,accepted_at,prepared_at,picked_at,order_total, set_time)
--- VALUES(1,now() + interval '20 seconds', now() + interval '2 minutes 20 seconds',now() + interval '3 minutes ' ,595, 2),
--- (2,now() + interval '40 seconds', now() + interval '5 minutes 40 seconds',now() + interval '6 minutes ' ,400, 5),
--- (1,now() + interval '44 seconds', now() + interval '5 minutes 44 seconds',now() + interval '6 minutes ' ,1395, 5),
--- (1,now() + interval '1 day 20 seconds', now() + interval '1 day 2 minutes 20 seconds',now() + interval '1 day 3 minutes ' ,1745, 2),
--- (1,now() + interval '2 day 20 seconds', now() + interval '2 day 2 minutes 20 seconds',now() + interval '2 day 3 minutes ' ,1225, 2),
--- (1,now() + interval ' 1 day 40 seconds', now() + interval '1 day 5 minutes 40 seconds',now() + interval ' 1 day 6 minutes ' ,1190, 5);
-
--- INSERT INTO orders (customer_id,order_total)
--- VALUES (1 ,400) ,(2,400),(2,400);
-
--- INSERT INTO orders (customer_id,order_total , accepted_at)
--- VALUES (1 ,400 ,now()) ;
--- INSERT INTO orders (customer_id,order_total , accepted_at,prepared_at)
--- VALUES (1 ,400 ,now(),now()) ;
-
 INSERT INTO menu_items (name,price,image_url)
-VALUES('Coffee',250,'https://www.sevensummitscoffee.com/uploads/1/3/1/2/131291290/s598870640510727702_p68_i1_w308.png'),
-('Tea',200,'https://i0.wp.com/pickuplineninja.com/wp-content/uploads/2020/05/tea-Pick-Up-Lines.jpeg?fit=500%2C344&ssl=1'),
-('Cookie',150,'https://upload.wikimedia.org/wikipedia/commons/f/f1/2ChocolateChipCookies.jpg'),
-('Sandwich',350,'https://static.toiimg.com/thumb/83740315.cms?width=1200&height=900'),
-('Muffin',350,'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimagesvc.meredithcorp.io%2Fv3%2Fmm%2Fimage%3Furl%3Dhttps%253A%252F%252Fstatic.onecms.io%252Fwp-content%252Fuploads%252Fsites%252F19%252F2011%252F04%252F08%252Fchocolate-chip-muffins-ck-2000.jpg&q=85'),
-('Omelette',450,'https://c.ndtvimg.com/2020-07/3cqv032o_omelette_625x300_23_July_20.jpg?im=FaceCrop,algorithm=dnn,width=1200,height=675');
+VALUES('Coffee',250,'https://media.istockphoto.com/photos/coffee-cup-isolated-picture-id1143290013?b=1&k=20&m=1143290013&s=170667a&w=0&h=AhZa7b7wOh1DLQGppJ0yaky9HJp0IWktRMPOp7hS_eo='),
+('Tea',200,'https://upload.wikimedia.org/wikipedia/commons/0/00/Mint_tea_-_Petr_Kratochvil.jpg'),
+('Cookie',150,'https://preview.pixlr.com/images/800wm/1208/2/1208201791.jpg'),
+('Sandwich',350,'https://thumbs.dreamstime.com/b/turkey-sandwich-white-background-11622814.jpg'),
+('Muffin',350,'https://thumbs.dreamstime.com/b/chocolate-chips-muffin-isolated-white-background-35031201.jpg'),
+('Omelette',450,'https://thumbs.dreamstime.com/b/omelette-vegetables-slices-tomato-cucumber-parsley-plate-light-shade-white-background-36085254.jpg');
 
--- INSERT INTO orders_items (order_id, menu_item_id, quantity)
--- VALUES (1,1,1),(1,2,1),(1,3,1),
--- (2,2,2),
--- (3,1,1),(3,4,2),(3,5,1),
--- (4,1,1),(4,3,10),
--- (5,1,5),
--- (6,4,2),(6,1,2),
--- (7,1,2),(8,1,2),
--- (9,1,2),(10,1,2),
--- (11,1,2);
-
---UPDATE orders SET STATUS ='Delivered' where id <=6;
